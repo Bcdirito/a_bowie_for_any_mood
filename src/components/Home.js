a8bb2672f891a13968de919f55f03467570b27b7
Bcdirito <bdirito91@gmail.com>
2019-10-29 12:06:22 -0400

    added react router

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = (props) => {
+        return (
+            <div>
+                <h1>David Bowie Mood Quiz</h1>
+                <button>Take the Quiz</button>
+                <footer>Wham Bam Thank You Ma'am</footer>
+            </div>
+        )
+    }
+}
