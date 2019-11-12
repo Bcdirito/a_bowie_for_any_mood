73b8fe82c23fa69846f97f4ad755b83b70d3af79
Bcdirito <bdirito91@gmail.com>
2019-11-11 23:08:46 -0500

    added hopeful music

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,17 @@ import React from 'react'
 import "../stylesheets/home.css"
 import {Link} from "react-router-dom"
 import homeBowie from "../media/images/home_page_bowie.jpg"
+import hopefulEgg from "../media/music/rock_n_roll_suicide.mp3"
 
 const Home = (props) => {
+    let audio;
+
+    const playHope = () => {
+        if (audio !== undefined) audio.pause()
+        audio = new Audio(hopefulEgg)
+        audio.play()
+    }
+
     return (
         <div className="homeContent">
             <h1>David Bowie Mood Quiz</h1>
@@ -12,6 +21,7 @@ const Home = (props) => {
             </div>
             <Link to="/quiz"><button>Take the Quiz</button></Link>
             <Link to="/about"><button>About Us</button></Link>
+            <button onClick={() => playHope()}>I'm Having a Really Tough Day</button>
         </div>
     )
 }
